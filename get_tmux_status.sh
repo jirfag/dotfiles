28d281cc1d5ee7b5c4c7f68fb60875e3b1fabbbe
Denis Isaev <d.isaev@corp.mail.ru>
2015-02-05 13:06:44 -0500

    improve status

--- get_tmux_status.sh
+++ get_tmux_status.sh
@@ -1,3 +1,4 @@
-CPU_USAGE=$(fgrep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {printf("%.1f%%", usage)}')
-MEM_USAGE=$(top -Mbn 1 | grep "Mem" | awk '{ print $4 }')
-echo "CPU: $CPU_USAGE, MEM: $MEM_USAGE"
+#CPU_USAGE=$(fgrep 'cpu ' /proc/stat | awk '{usage=($2+$4)*100/($2+$4+$5)} END {printf("%.1f%%", usage)}')
+CPU_USAGE=$(top -bn 2 -d 0.01 | fgrep "Cpu(s):" | tail -1 | gawk '{print $2+$4+$6}')
+MEM_USAGE=$(top -Mbn 2 -d 0.01 | fgrep "Mem:" | tail -1 | gawk '{ print $4 }')
+echo "CPU: ${CPU_USAGE}%, MEM: $MEM_USAGE"
