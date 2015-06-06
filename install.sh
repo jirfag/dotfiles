fac216f128ae3c453b1e9c378d11d69ca4f355dc
Denis Isaev <d.isaev@corp.mail.ru>
2015-06-06 13:14:38 +0000

    improve installation script, check it fulle on ubutu 14.04

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
+STATUS_FILE="./status.txt"
+
 function inst() {
     test -f ~/$1 && rm -f ~/$1.bak && mv ~/$1 ~/$1.bak
     ln $1 ~/$1
@@ -16,6 +18,12 @@ if [ ! -d "$VUNLDE_INST_DIR" ]; then
     git clone https://github.com/gmarik/Vundle.vim.git $VUNLDE_INST_DIR
 fi
 
+if echo "$(uname -a)" | grep -qi ubuntu; then
+    echo "install ubuntu packages"
+    CMD="sudo apt-get install -y exuberant-ctags npm build-essential cmake python-dev"
+    eval $CMD || { sudo apt-get update; eval $CMD; }
+fi
+
 vim +PluginInstall +qall
 
 if [ ! -f ~/.vim/colors/solarized.vim ]; then
@@ -23,11 +31,15 @@ if [ ! -f ~/.vim/colors/solarized.vim ]; then
     ln ~/.vim/bundle/vim-colors-solarized/colors/solarized.vim ~/.vim/colors/solarized.vim
 fi
 
-if [ ! "$(ls -A ~/.vim/bundle/YouCompleteMe)" ]; then
-    cd ~/.vim/bundle/YouCompleteMe
-    ./install.sh --clang-completer
+if [ -f ~/.vim/bundle/YouCompleteMe/install.sh ]; then
+    if ! grep -q ycm $STATUS_FILE; then
+        echo "install YCM core"
+        cd ~/.vim/bundle/YouCompleteMe
+        ./install.sh --clang-completer
+        echo ycm >>$STATUS_FILE
+    fi
 fi
 
 sudo npm install -g jslint # for syntastic for vim
-rm -f ~/bin/get_tmux_status.sh
-sudo ln get_tmux_status.sh /usr/bin/get_tmux_status.sh
+sudo rm -f /usr/bin/get_tmux_status.sh
+sudo ln ./get_tmux_status.sh /usr/bin/get_tmux_status.sh
