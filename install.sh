ec6fb7b39ec50729c4c65155a726de3ad5333fc7
Denis Isaev <d.isaev@corp.mail.ru>
2016-01-16 20:44:18 +0300

    improve install script

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 STATUS_FILE="./status.txt"
 
 function inst() {
-    test -f ~/$1 && rm -f ~/$1.bak && mv ~/$1 ~/$1.bak
-    ln $1 ~/$1
     echo "ln $1 ~/$1"
+    test -f ~/$1 && rm -f ~/$1.bak && mv ~/$1 ~/$1.bak
+    ln $1 ~/$1 || { echo "can't do ln"; exit 1; }
 }
 
 inst .bashrc
@@ -24,7 +24,7 @@ if echo "$(uname -a)" | grep -qi ubuntu; then
     eval $CMD || { sudo apt-get update; eval $CMD; }
 fi
 
-vim +PluginInstall +qall
+vim +PluginInstall +qall || { echo "can't install vim plugins"; exit 1; }
 
 if [ ! -f ~/.vim/colors/solarized.vim ]; then
     mkdir -p ~/.vim/colors
@@ -37,8 +37,13 @@ if [ -f ~/.vim/bundle/YouCompleteMe/install.sh ]; then
         (cd ~/.vim/bundle/YouCompleteMe && ./install.sh --clang-completer --system-libclang)
         echo ycm >>$STATUS_FILE
     fi
+else
+    echo "YCM wasn't installed"
+    exit 1
 fi
 
-sudo npm install -g jslint # for syntastic for vim
+echo "install jslint..."
+npm list -g jslint || sudo npm install -g jslint # for syntastic for vim
+
 sudo rm -f /usr/bin/get_tmux_status.sh
 sudo ln ./get_tmux_status.sh /usr/bin/get_tmux_status.sh
